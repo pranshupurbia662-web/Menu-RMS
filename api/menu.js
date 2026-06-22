9b72c4397b5e92e2d78d5416616e234bfbe839ae
pranshupurbia662-web <pranshupurbia662@gmail.com>
2026-06-22 22:50:48 +0530

    update

--- api/menu.js
+++ api/menu.js
@@ -1,26 +1,5 @@
-import connectDB from "../Backend/config/Db.js";
-import Menu from "../Backend/models/MenuModel.js";
-
-export default async function handler(req, res) {
-
-  await connectDB();
-
-  if (req.method === "GET") {
-
-    try {
-
-      const menuItems = await Menu.find();
-
-      res.status(200).json(menuItems);
-
-    } catch (error) {
-
-      res.status(500).json({
-        message: error.message,
-      });
-
-    }
-
-  }
-
+export default function handler(req, res) {
+  res.status(200).json({
+    message: "API Working"
+  });
 }
